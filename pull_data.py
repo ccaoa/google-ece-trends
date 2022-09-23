9fed0f1725e0c7a5c5d72dc182a9342aa4dcf94b
J. A. Cooper <Alex.Cooper@usa.childcareaware.org>
2022-09-23 17:55:32 -0400

    Consolidate results formatting code into one function that can accept spatial or temporal trend DFs.

--- pull_data.py
+++ pull_data.py
@@ -78,7 +78,63 @@ def payload_builder(timeframe=None, geography_broad='US', search_item=ece_topic_
 #     # payload.related_queries()
 #     # payload.get_historical_interest(keywords)
 
-def extract_temporal_data(payload=None):
+
+def gtis_df_formatter(payload_df, search_term, uoa, rank_sort=True):
+    """ Generates a clean DF with a formatted GTIS column for either time or geography results
+    with an optional ranking column for each UOA region. """
+
+    # Rename the Google Trends Interest Score (GTIS) column
+    score_column_name = 'gtis'
+    payload_df =payload_df.rename(columns={search_term: score_column_name})
+    # The date/geography variable item is set by default as a pandas DF index. We want it as an actual, physical column.
+    # # If temporal, UOA should be = 'date'
+    # # # (See if this differs as time period for results differs. I.e. if UOA is a week vs a day). If it does, see spatial function way of resolving multiple possible UOAs.
+    # # If spatial, UOA will vary: uoa_resolutions =['COUNTRY','REGION','DMA','CITY']
+    payload_df[uoa] = payload_df.index
+    # Set UOA col as first column
+    cols = payload_df.columns.to_list()
+    cols = cols[-1:] + cols[:-1]
+    payload_df = payload_df[cols]
+    # Reset the index to be numerical.
+    payload_df = payload_df.reset_index(drop=True)
+
+    # Add a rank to the dataframe. Only sort if geography.
+    # if rank is True:
+    rank_dataframe = pd.DataFrame(list(range(1,len(payload_df)+1)), columns=['rank'])
+    # Include the rankings in the payload dictionary
+    if rank_sort is True:
+        payload_df = pd.concat(
+            # Sorted version of the GTrends Data
+            [payload_df.sort_values(score_column_name, ascending=False).reset_index(drop=True),
+             # Merge regional data with its GTIS ranking
+             rank_dataframe]#.astype(int)]  # Strangely, setting astype to int gives it a decimal...
+            , axis=1
+        )
+    else:
+        # Rank results without sorting.
+        dfforrank = pd.concat(
+            # Sorted version of the GTrends Data
+            # # Don't reset the index. Generates its own 'index' column to reference later.
+            [payload_df.sort_values(score_column_name, ascending=False).reset_index(False),
+             # Merge regional data with its GTIS ranking
+             rank_dataframe]#.astype(int)]
+            , axis=1
+        )
+        # Get your DF in it's OG order again.
+        dfforrank = dfforrank.sort_values('index', ascending=True)
+        # payload_df = dfforrank.drop('index')
+        payload_df = dfforrank.set_index('index')
+        del dfforrank
+
+    del rank_dataframe
+
+    # Set the UOA column as the first in the df
+    payload_df = payload_df[[uoa] + [col for col in payload_df.columns if col != uoa]]
+
+    return payload_df
+
+
+def extract_temporal_data(payload=None, time_uoa='date'):
     """ Pass a connection item with a built payload into this function to extract meaningful time data from it. """
 
     if payload is None:
@@ -89,20 +145,21 @@ def extract_temporal_data(payload=None):
     time_df = payload.interest_over_time()
     #
     # Format the new DF to make it easier to read
-    time_df=time_df.rename(columns={ece_topic_code: "gtis"})
-
-    # The region item is set by default as a pandas DF index. We want it as an actual column.
-    date_column = 'date'
-    # Add the geog index as a column to the df
-    time_df[date_column] = time_df.index
-    # Move geog col as first column
-    timecolumns = time_df.columns.to_list()
-    timecolumns = timecolumns[-1:] + timecolumns[:-1]
-    time_df = time_df[timecolumns]
-    # Reset the index to be numerical.
-    time_df = time_df.reset_index(drop=True)
-    # Set the date column as the first in the df
-    time_df = time_df[[date_column] + [col for col in time_df.columns if col != date_column]]
+    # time_df=time_df.rename(columns={ece_topic_code: "gtis"})
+    #
+    # # The date item is set by default as a pandas DF index. We want it as an actual column.
+    # date_column = 'date'
+    # # Add the geog index as a column to the df
+    # time_df[date_column] = time_df.index
+    # # Move geog col as first column
+    # timecolumns = time_df.columns.to_list()
+    # timecolumns = timecolumns[-1:] + timecolumns[:-1]
+    # time_df = time_df[timecolumns]
+    # # Reset the index to be numerical.
+    # time_df = time_df.reset_index(drop=True)
+    # # Set the date column as the first in the df
+    # time_df = time_df[[date_column] + [col for col in time_df.columns if col != date_column]]
+    time_df = gtis_df_formatter(time_df, ece_topic_code, time_uoa, rank_sort=False)
 
     return time_df
 
@@ -142,51 +199,57 @@ def extract_spatial_data(payload=None,subregion="REGION", low_volume=True):
     geog_df = payload.interest_by_region(resolution=subregion, inc_low_vol=low_volume)
 
     # Format the new DF to make it easier to read
-    score_column_name = 'gtis'
-    geog_df=geog_df.rename(columns={ece_topic_code: score_column_name})
-    # The region item is set by default as a pandas DF index. We want it as an actual column.
+    # score_column_name = 'gtis'
+    # geog_df=geog_df.rename(columns={ece_topic_code: score_column_name})
+    # The region item is set by default as a pandas DF index. Identify that name.
     region_column = subregion.lower()
     if region_column=='region':
         region_column='state'
-    # Add the geog index as a column to the df
-    geog_df[region_column] = geog_df.index
-    # Move geog col as first column
-    colsreg = geog_df.columns.to_list()
-    colsreg = colsreg[-1:] + colsreg[:-1]
-    geog_df = geog_df[colsreg]
-    # Reset the index to be numerical.
-    geog_df = geog_df.reset_index(drop=True)
-    # We also want the regions in the df to have a rank
-    # # Generate a plain, functional DF with a ranking of 1 to n(columns in region DF)
-    rank_col = "rank"
-    rank_df = pd.DataFrame(list(range(1, len(geog_df))), columns=[rank_col])
-    geog_df = pd.concat(
-        # Sorted version of the GTrends Data
-        [geog_df.sort_values(score_column_name, ascending=False).reset_index(drop=True),
-         # Merged with a ranking
-         rank_df.astype(int)]
-        , axis=1
-    )
-    del rank_df  # Take the scratch rank dataframe out of memory.
-    # Set the date column as the first in the df
-    geog_df = geog_df[[rank_col] + [col for col in geog_df.columns if col != rank_col]]
-
+    # # Add the geog index as a column to the df
+    # geog_df[region_column] = geog_df.index
+    # # Move geog col as first column
+    # colsreg = geog_df.columns.to_list()
+    # colsreg = colsreg[-1:] + colsreg[:-1]
+    # geog_df = geog_df[colsreg]
+    # # Reset the index to be numerical.
+    # geog_df = geog_df.reset_index(drop=True)
+    # # We also want the regions in the df to have a rank
+    # # # Generate a plain, functional DF with a ranking of 1 to n(columns in region DF)
+    # rank_col = "rank"
+    # rank_df = pd.DataFrame(list(range(1, len(geog_df))), columns=[rank_col])
+    # geog_df = pd.concat(
+    #     # Sorted version of the GTrends Data
+    #     [geog_df.sort_values(score_column_name, ascending=False).reset_index(drop=True),
+    #      # Merged with a ranking
+    #      rank_df.astype(int)]
+    #     , axis=1
+    # )
+    # del rank_df  # Take the scratch rank dataframe out of memory.
+    # # Set the date column as the first in the df
+    # geog_df = geog_df[[rank_col] + [col for col in geog_df.columns if col != rank_col]]
+    geog_df = gtis_df_formatter(geog_df, ece_topic_code, region_column, rank_sort=True)
 
     return geog_df
 
 
 if __name__ == '__main__':
-    import time
-    start = time.time()
+    from time import time, sleep
+    start = time()
 
     # Beware of timeout requests:
     # `pytrends.exceptions.ResponseError: The request failed: Google returned a response with code 429.`
     # https://stackoverflow.com/questions/50571317/pytrends-the-request-failed-google-returned-a-response-with-code-429
-    payload = payload_builder()  # Default args  # Future: pass your timeframe argument into this func.
-    states_df = extract_spatial_data(payload, subregion='states')
+    the_payload = payload_builder()  # Default args  # Future: pass your timeframe argument into this func.
+    states_df = extract_spatial_data(the_payload, subregion='states')
+    sleep(2)  # 2 second pause to trick the Google API?
     temporal_df = extract_temporal_data()
+    sleep(2)  # 2 second pause to trick the Google API?
+    dma_df = extract_spatial_data(the_payload, subregion='DMA')
 
     print(states_df.head(10))
+    print(states_df.tail(10))
     print(temporal_df.head(10))
+    print(dma_df.head(10))
+    print(dma_df.tail(10))
 
     core.runtime(start)
