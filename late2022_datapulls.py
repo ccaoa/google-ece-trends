c396df17e632023dddeec637e8232a40bdfe0207
J. A. Cooper <jacooper1317@gmail.com>
2022-10-10 14:34:23 -0400

    Full run without Google 429 errors. Some output duplications/inconsistencies still need to be addressed though.

--- late2022_datapulls.py
+++ late2022_datapulls.py
@@ -14,22 +14,23 @@ def full_run_gtrends():
     # `pytrends.exceptions.ResponseError: The request failed: Google returned a response with code 429.`
     # https://stackoverflow.com/questions/50571317/pytrends-the-request-failed-google-returned-a-response-with-code-429
     google_connection = pull.connect_to_gtrends()
+    sleep(11)  # >10 second pause to trick the Google API?
     # USA pulls
     # # Remember, the payload is where you pass your study time-period argument
     init_late2022_studyperiod = '2018-06-03 2022-09-10'  # The COVID Valentines' study period.
     geog_usa = 'US'
     # Build the payload
     usa_payload = pull.payload_builder(timeframe=init_late2022_studyperiod, geography_broad=geog_usa, connection_item=google_connection)  # Default args  # Future: pass your timeframe argument into this func.
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     #
     # Use the extract_data(payload_item, spatial_not_temporal=True, region=None, low_volume=True) func
     # to pull relevant data + formatting out of the payload.
     usa_states_df = pull.extract_data(usa_payload, region='states', spatial_not_temporal=True)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     usa_temporal_df = pull.extract_data(usa_payload, spatial_not_temporal=False)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     usa_dma_df = pull.extract_data(usa_payload, spatial_not_temporal=True,region='DMA')
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     #
     # Get related queries
     usa_top_qs = pull.top_related_queries(usa_payload)
@@ -41,39 +42,39 @@ def full_run_gtrends():
     geog_oh = 'US-OH'
     # Build the payloads
     ky_payload = pull.payload_builder(geography_broad=geog_ky, timeframe=init_late2022_studyperiod, connection_item=google_connection)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     in_payload = pull.payload_builder(geography_broad=geog_in, timeframe=init_late2022_studyperiod, connection_item=google_connection)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     oh_payload = pull.payload_builder(geography_broad=geog_oh, timeframe=init_late2022_studyperiod, connection_item=google_connection)
-    sleep(2)
+    sleep(11)
     #
     # Pull relevant data
     ky_dma = pull.extract_data(payload_item=ky_payload, spatial_not_temporal=True, region='DMA')
-    sleep(2)
+    sleep(11)
     ky_time = pull.extract_data(payload_item=ky_payload, spatial_not_temporal=True)
-    sleep(2)
+    sleep(11)
     in_dma = pull.extract_data(payload_item=in_payload, spatial_not_temporal=True, region='DMA')
-    sleep(2)
+    sleep(11)
     in_time = pull.extract_data(payload_item=in_payload, spatial_not_temporal=True)
-    sleep(2)
+    sleep(11)
     oh_dma = pull.extract_data(payload_item=oh_payload, spatial_not_temporal=True, region='DMA')
-    sleep(2)
+    sleep(11)
     oh_time = pull.extract_data(payload_item=oh_payload, spatial_not_temporal=True)
-    sleep(2)
+    sleep(11)
 
 
     # Also include some data from the classic COVID Valentines' study period.
     # Augment the OR & MN data with some national spatial data + get some riding and top keywords.
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     valentines_period = '2020-02-14 2021-02-14'
     # Build the payload
     valentines_usa_payload = pull.payload_builder(valentines_period, geography_broad=geog_usa, connection_item=google_connection)
     valentines_states_df = pull.extract_data(valentines_usa_payload, region='states', spatial_not_temporal=True)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     valentines_temporal_df = pull.extract_data(valentines_usa_payload, spatial_not_temporal=False)
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     valentines_dma_df = pull.extract_data(valentines_usa_payload, spatial_not_temporal=True,region='DMA')
-    sleep(2)  # 2 second pause to trick the Google API?
+    sleep(11)  # >10 second pause to trick the Google API?
     #
     # Get related queries
     valentines_top_qs = pull.top_related_queries(valentines_usa_payload)
